7e35d9d77b9a0be74e8f3c7290f545f2c748fa02
NahDang <23520037@gm.uit.edu.vn>
2024-09-13 12:01:09 +0700

    NT106

---
+++ PictureViewer/PictureViewer/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog o= new OpenFileDialog();
+            o.Title = "File Explore";
+            o.Filter = "File anh (*.pnq , *.gif , *.jpg)|*.pnq ; *.gif;*.jpg|All file|*.*";
+            if(o.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image=Image.FromFile(o.FileName);
+            }
+
+        }
+    }
+}
