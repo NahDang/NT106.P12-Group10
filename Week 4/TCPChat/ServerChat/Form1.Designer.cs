e16e6b9942a838ab57b877de614826e283f14c9b
Hoang05UIT <23520524@gm.uit.edu.vn>
2024-10-02 21:42:59 +0700

    W4

---
+++ Week 4/TCPChat/ServerChat/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ServerChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIP.Location = new System.Drawing.Point(70, 36);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(244, 34);
+            this.txtIP.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(502, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Bắt đầu/Kết thúc";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 105);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(741, 306);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Week 4/TCPChat/ServerChat/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ServerChat
+{
+    public partial class Form1 : Form
+    {
+        private Thread listenThread;
+        private Thread fileThread;
+        private TcpListener tcpListener;
+        private TcpListener fileListener;
+        private bool stopChatServer = true;
+        private readonly int _serverPort = 8000; 
+        private readonly int _filePort = 8001;   
+        private Dictionary<string, TcpClient> dict = new Dictionary<string, TcpClient>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Parse(txtIP.Text), _serverPort);
+                tcpListener.Start();
+
+                while (!stopChatServer)
+                {
+                    TcpClient _client = tcpListener.AcceptTcpClient();
+
+                    StreamReader sr = new StreamReader(_client.GetStream());
+                    StreamWriter sw = new StreamWriter(_client.GetStream());
+                    sw.AutoFlush = true;
+                    string username = sr.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(username))
+                    {
+                        sw.WriteLine("Vui lòng chọn tên người dùng");
+                    }
+                    else
+                    {
+                        if (!dict.ContainsKey(username))
+                        {
+                            Thread clientThread = new Thread(() => ClientRecv(username, _client));
+                            dict.Add(username, _client);
+                            clientThread.Start();
+                            UpdateChatHistoryThreadSafe($"User {username} connected.\n");
+                        }
+                        else
+                        {
+                            sw.WriteLine("Tên người dùng đã tồn tại, hãy chọn tên khác");
+                        }
+                    }
+                }
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void ListenForFiles()
+        {
+            try
+            {
+                fileListener = new TcpListener(IPAddress.Parse(txtIP.Text), _filePort);
+                fileListener.Start();
+
+                while (!stopChatServer)
+                {
+                    TcpClient fileClient = fileListener.AcceptTcpClient();
+                    Thread receiveFileThread = new Thread(() => ReceiveFile(fileClient));
+                    receiveFileThread.Start();
+                }
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void ReceiveFile(TcpClient client)
+        {
+            try
+            {
+                NetworkStream ns = client.GetStream();
+                StreamReader sr = new StreamReader(ns);
+
+
+                string fileName = sr.ReadLine();
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = ns.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        ms.Write(buffer, 0, bytesRead);
+                    }
+
+                    
+                    UpdateChatHistoryThreadSafe($"Received file: {fileName} with size: {ms.Length} bytes\n");
+                }
+
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void ClientRecv(string username, TcpClient tcpClient)
+        {
+            StreamReader sr = new StreamReader(tcpClient.GetStream());
+            try
+            {
+                while (!stopChatServer)
+                {
+                    string msg = sr.ReadLine();
+                    string formattedMsg = $"[{DateTime.Now:MM/dd/yyyy h:mm tt}] {username}: {msg}\n";
+
+                    foreach (TcpClient otherClient in dict.Values)
+                    {
+                        StreamWriter sw = new StreamWriter(otherClient.GetStream());
+                        sw.WriteLine(formattedMsg);
+                        sw.AutoFlush = true;
+                    }
+
+                    UpdateChatHistoryThreadSafe(formattedMsg);
+                }
+            }
+            catch (SocketException)
+            {
+                tcpClient.Close();
+                sr.Close();
+            }
+        }
+        private delegate void SafeCallDelegate(string text);
+        private void UpdateChatHistoryThreadSafe(string text)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(UpdateChatHistoryThreadSafe);
+                richTextBox1.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                richTextBox1.Text += text;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (stopChatServer)
+            {
+                stopChatServer = false;
+                listenThread = new Thread(Listen);
+                fileThread = new Thread(ListenForFiles);
+                listenThread.Start();
+                fileThread.Start();
+                MessageBox.Show("Máy chủ đã khởi động, đang lắng nghe kết nối.");
+                button1.Text = "Dừng";
+            }
+            else
+            {
+                stopChatServer = true;
+                tcpListener.Stop();
+                fileListener.Stop();
+                listenThread = null;
+                fileThread = null;
+                dict.Clear();
+                MessageBox.Show("Máy chủ đã dừng.");
+                button1.Text = "Bắt đầu nghe.";
+            }
+        }
+    }
+}
