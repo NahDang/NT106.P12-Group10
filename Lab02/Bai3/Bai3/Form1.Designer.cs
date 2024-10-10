d17972966f194b8780f8191088bac9e6b37ba37e
NahDang <23520037@gm.uit.edu.vn>
2024-10-10 09:54:54 +0700

    10/10

---
+++ Lab02/Bai3/Bai3/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Bai3
+{
+    partial class Bai03
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readfile = new System.Windows.Forms.Button();
+            this.writefile = new System.Windows.Forms.Button();
+            this.rtbRead = new System.Windows.Forms.TextBox();
+            this.rtbWrite = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // readfile
+            // 
+            this.readfile.Location = new System.Drawing.Point(22, 44);
+            this.readfile.Name = "readfile";
+            this.readfile.Size = new System.Drawing.Size(218, 123);
+            this.readfile.TabIndex = 0;
+            this.readfile.Text = "Read File";
+            this.readfile.UseVisualStyleBackColor = true;
+            this.readfile.Click += new System.EventHandler(this.readfile_Click);
+            // 
+            // writefile
+            // 
+            this.writefile.Location = new System.Drawing.Point(22, 360);
+            this.writefile.Name = "writefile";
+            this.writefile.Size = new System.Drawing.Size(218, 123);
+            this.writefile.TabIndex = 1;
+            this.writefile.Text = "Write File";
+            this.writefile.UseVisualStyleBackColor = true;
+            this.writefile.Click += new System.EventHandler(this.writefile_Click);
+            // 
+            // rtbRead
+            // 
+            this.rtbRead.Location = new System.Drawing.Point(257, 44);
+            this.rtbRead.Multiline = true;
+            this.rtbRead.Name = "rtbRead";
+            this.rtbRead.Size = new System.Drawing.Size(728, 257);
+            this.rtbRead.TabIndex = 4;
+            // 
+            // rtbWrite
+            // 
+            this.rtbWrite.Location = new System.Drawing.Point(257, 360);
+            this.rtbWrite.Multiline = true;
+            this.rtbWrite.Name = "rtbWrite";
+            this.rtbWrite.Size = new System.Drawing.Size(728, 257);
+            this.rtbWrite.TabIndex = 5;
+            // 
+            // Bai03
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1965, 763);
+            this.Controls.Add(this.rtbWrite);
+            this.Controls.Add(this.rtbRead);
+            this.Controls.Add(this.writefile);
+            this.Controls.Add(this.readfile);
+            this.Name = "Bai03";
+            this.Text = "Bai03";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readfile;
+        private System.Windows.Forms.Button writefile;
+        private System.Windows.Forms.TextBox rtbRead;
+        private System.Windows.Forms.TextBox rtbWrite;
+    }
+}
+
---
+++ Lab02/Bai3/Bai3/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bai3
+{
+    public partial class Bai03 : Form
+    {
+        public Bai03()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog ofd;
+
+        private void readfile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ofd = new OpenFileDialog();
+                ofd.Filter = "Text Files|*.txt";
+                ofd.ShowDialog();
+                FileStream fs = new FileStream(ofd.FileName, FileMode.OpenOrCreate);
+                StreamReader sr = new StreamReader(fs);
+                rtbRead.Text = sr.ReadToEnd();
+                sr.BaseStream.Position = 0;
+                writefile.Enabled = true;
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi! Không đọc được file này. " + ex.Message);
+            }
+        }
+
+        private void writefile_Click(object sender, EventArgs e)
+        {
+                File.WriteAllText("..\\..\\output3.txt", ""); 
+                FileStream fsw = new FileStream("..\\..\\output3.txt", FileMode.OpenOrCreate);
+                StreamWriter sw = new StreamWriter(fsw, Encoding.UTF8);
+                FileStream fsr = new FileStream(ofd.FileName, FileMode.OpenOrCreate);
+                StreamReader sr = new StreamReader(fsr, Encoding.UTF8);
+
+                string[] res = Calc(sr); 
+                foreach (var item in res)
+                {
+                    sw.WriteLine(item); 
+                }
+
+                sw.Close();
+                rtbWrite.Text = File.ReadAllText(fsw.Name); 
+                fsw.Close();
+                fsr.Close();
+        }
+
+        private string[] Calc(StreamReader sr)
+        {
+            try
+            {
+                var content = sr.ReadToEnd();
+                var Lines = content.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+
+                for (int i = 0; i < Lines.Length; i++)
+                {
+                    if (Lines[i].Contains('+') || Lines[i].Contains('-') || Lines[i].Contains('*') || Lines[i].Contains('/'))
+                    {
+                        string[] tokens = Lines[i].Split(new char[] { '+', '-', '*', '/' });
+                        List<decimal> numbers = new List<decimal>(); ;
+                        List<char> operators = new List<char>();
+                        foreach (var token in tokens)
+                        {
+                            decimal number;
+                            if (decimal.TryParse(token, out number))
+                            {
+                                numbers.Add(number);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Dữ liệu nhập vào không chính xác theo yêu cầu đề bài!");
+                                return new string[] { };
+                            }
+                        }
+                        for (int j = 0; j < Lines[i].Length; j++)
+                        {
+                            if (Lines[i][j] == '+' || Lines[i][j] == '-' || Lines[i][j] == '*' || Lines[i][j] == '/')
+                            {
+                                operators.Add(Lines[i][j]);
+                            }
+                        }
+                        decimal result = numbers[0];
+                        for (int j=1;j<numbers.Count;j++)
+                        {
+                            if (operators[j - 1] == '+') result += numbers[j];
+                            else if (operators[j - 1] == '-') result -= numbers[j];
+                            else if (operators[j - 1] == '*') result *= numbers[j];
+                            else if (operators[j - 1] == '/') result /= numbers[j];
+                        }
+                        Lines[i] = Lines[i] + " = " + result.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Dữ liệu nhập vào không chính xác theo yêu cầu đề bài!");
+                    }
+                }
+
+                return Lines;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi! Không thể tính toán. " + ex.Message);
+                return new string[] { };
+            }
+        }
+    }
+}
